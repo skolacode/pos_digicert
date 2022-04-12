c5979f25d536935342c7875b7b1c228e609417cb
Mohamed Burhan <bern2727@gmail.com>
2022-04-12 10:49:50 +0800

    1. update the margin style

--- src/styles/formInputStyle.js
+++ src/styles/formInputStyle.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 export const formInputStyle = StyleSheet.create({
   input: {
     height: 40,
-    margin: 12,
+    margin: 15,
     borderWidth: 1,
     padding: 10,
   },
